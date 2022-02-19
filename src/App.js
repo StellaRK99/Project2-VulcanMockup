943d9d43f4eeaef8a96ca2ff2c1a5898656158e8
stella kagrimanyan <stella.kagri@gmail.com>
2022-02-19 12:41:03 -0800

    Loaded AboutBanner component into Appjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import MainBanner from './components/MainBanner';
 import Navbar from './components/Navbar'
-
+import AboutBanner from './components/AboutBanner'
 
 function App() {
   console.log("test")
   return (
     <div className="App">
       <MainBanner />
+      <AboutBanner />
     </div>
   );
 }
