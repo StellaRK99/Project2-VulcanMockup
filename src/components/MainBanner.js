349db2c19b67c05459441b0ab8866513ac2b7c3e
Brennan <brennan1086@gmail.com>
2022-04-02 15:22:39 -0700

    Moblie styling done along with burger btn is fully functional

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect  } from 'react'
 import BannerCarousel from './Carousel'
 import Icon from './Icon'
 import Navbar from './Navbar'
 
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+ function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 function MainBanner() {
+  const [toggle, setToggle] = useState(false)
+  const { height, width } = useWindowDimensions();
+  console.log(width)
+  useEffect(()=>{
+    if(width >= 767){
+      console.log('wewewewewewe')
+      setToggle(false)
+    }
+  }, [width])
+  
+
   return (
     <div className='MainBanner'>
         <ul className='TopNavbar'>
@@ -23,17 +57,33 @@ function MainBanner() {
           <li>
             <h4>CONTACT US</h4>
           </li>
-          <div className='BurgerBtnContainer'>
+          <div className='BurgerBtnContainer' onClick={() => setToggle(!toggle)}>
             <div className='BurgerBtn'></div>
             <div className='BurgerBtn'></div>
             <div className='BurgerBtn'></div>
           </div>
         </ul>
+        {
+          toggle && <ul className='MobileNav'>
+            <li><a href='/'>ABOUT</a></li>
+            <li><a href='/'>PRODUCTS</a></li>
+            <li><a href='/'>SERVICES</a></li>
+            <li><a href='/'>PARTS</a></li>
+            <li><a href='/'>NEWS</a></li>
+            <li><a href='/'>CONTACT</a></li>
+            <li><a href='/'>INSTALLATION MAP</a></li>
+            <li><a href='/'>PRODUCT LITERATURE</a></li>
+          </ul>
+        }
         <Navbar />
         <h1>WE DESIGN AND FABRICATE INDESTRUCTIBLE WASTEWATER SCREENS. ENOUGH SAID.</h1>
         <div className='ButtonContainer'>
-          <button className='Prosucts btn'>VIEW PRODUCTS</button>
-          <button className='Services btn'>VIEW SERVICES</button>
+          <div className='Holder HL'>
+            <button className='Prosucts btn'>VIEW PRODUCTS</button>
+          </div>
+          <div className='Holder HR'>
+            <button className='Services btn'>VIEW SERVICES</button>
+          </div>
         </div>
         <BannerCarousel />
         
